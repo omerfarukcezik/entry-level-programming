73c849b7fd081cc562d3b107d8c3d278c5c0fee9
omerfarukcezik <omerfarukcezik@gmail.com>
2021-11-06 20:14:10 +0300

    finished

---
+++ src/_010_multiplyexample.java
@@ -0,0 +1,16 @@
+public class _010_multiplyexample {
+    public static void main(String[] args) {
+        int a, b, c;
+        Double x, y;
+        a = 2;
+        c = 3;
+        x = 4.2;
+
+        b = a * c;
+        System.out.println("Int çarpı Int budur : " + a);
+        y = a * x;
+        System.out.println("Int ve double çarpımı budur :" + y);
+
+
+    }
+}
