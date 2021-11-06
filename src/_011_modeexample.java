abeb5a13af908e4573f0f4665aa8d0a677af8b99
omerfarukcezik <omerfarukcezik@gmail.com>
2021-11-06 20:23:36 +0300

    finished

---
+++ src/_011_modeexample.java
@@ -0,0 +1,18 @@
+public class _011_modeexample {
+    public static void main(String[] args) {
+        int a, b, c, d;
+
+        a = 5;
+        b = 7;
+        c = 9;
+        d = 2;
+        System.out.println("a % b = " + a % b);
+        System.out.println("a % c = " + a % c);
+
+        System.out.println("d % c = " + d % c);
+        System.out.println("a % 1 = " + a % 1);
+        System.out.println("a % 5 = " + a % 5);
+        System.out.println("a % 10 = " + a % 10);
+
+    }
+}
