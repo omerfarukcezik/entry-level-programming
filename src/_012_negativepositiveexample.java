ca322d37d062a3cc17d0a48964eb0836f5039174
omerfarukcezik <omerfarukcezik@gmail.com>
2021-11-06 20:31:44 +0300

    finished

---
+++ src/_012_negativepositiveexample.java
@@ -0,0 +1,16 @@
+public class _012_negativepositiveexample {
+    public static void main(String[] args) {
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        int n;
+
+        System.out.print("Bir sayı giriniz : ");
+        n = kb.nextInt();
+
+        if(n<0)
+        System.out.println("Negatif");
+        else
+        System.out.println("Pozitif");
+
+    }
+}
