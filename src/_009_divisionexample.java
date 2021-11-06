95157e53c3d69200779aa3a7578eca251ef0a5a5
omerfarukcezik <omerfarukcezik@gmail.com>
2021-11-06 20:09:10 +0300

    finished

---
+++ src/_009_divisionexample.java
@@ -0,0 +1,14 @@
+public class _009_divisionexample {
+    public static void main(String[] args) {
+        int a, b, r;
+        double c, o;
+        a = 14;
+        b = 7;
+        c = 2.5;
+        r = a / b;
+        System.out.println("Tam sayı ile bölümden çıkan sonuç budur : " + r);
+        o = a / c ;
+        System.out.println("Ondalıklı sayıdan çıkan sonuç budur : " +o );
+
+    }
+}
