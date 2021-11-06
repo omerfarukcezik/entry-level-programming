e14ebdd96f62146084470dc1efdb9ff1c06c81cc
omerfarukcezik <omerfarukcezik@gmail.com>
2021-11-06 19:29:38 +0300

    finished

---
+++ src/_006_twotimesnumber.java
@@ -0,0 +1,10 @@
+public class _006_twotimesnumber {
+    public static void main(String[] args) {
+        int a;
+
+        a = 20;
+        a = a * 2;
+        System.out.println("a'nın iki katı :"+ a);
+
+    }
+}
