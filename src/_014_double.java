1e512f76d26a666325f8abf0730bb3472b2cb74d
omerfarukcezik <omerfarukcezik@gmail.com>
2021-11-12 21:36:27 +0300

    finished

---
+++ src/_014_double.java
@@ -0,0 +1,18 @@
+public class _014_double {
+    public static void main(String[] args) {
+        double x ,y , z;
+
+        x = 23.5;
+        System.out.println("x =" + x);
+
+        y = 7.3;
+        System.out.println("y =" + y);
+
+        z = x / y;
+        System.out.println("x / y =" + z);
+
+        z = x % y;
+        System.out.println("x % y =" + z);
+
+    }
+}
