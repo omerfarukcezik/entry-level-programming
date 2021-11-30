655b43002cb45cdd3f6589674d74f73ca0a4cfe2
omerfarukcezik <omerfarukcezik@gmail.com>
2021-11-30 22:45:28 +0300

    finished

--- src/_025_findingvalue.java
+++ src/_025_findingvalue.java
@@ -6,20 +6,20 @@ public class _025_findingvalue {
         System.out.print("x'i giriniz :");
         x = kb.nextInt();
 
-        y = k * x + t;
-
         if (x < 0) {
             k = 2;
             t = 0;
 
-        }
-        else
-            if (x <= 5) {
-                k = 3;
-                t = -20 ;
+        } else if (x <= 5) {
+            k = -1;
+            t = 10;
 
-            }
-            y = k * x + t;
-            System.out.println("y :" + y);
+        } else {
+            k = 3;
+            t = -20;
+        }
+        y = k * x + t;
+        System.out.println("y :" + y);
     }
+
 }
